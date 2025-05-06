235e85a2eadd690d012519087624ba22387dd18e
blueberry.buns <hulewicz.k@gmail.com>
2025-05-06 10:51:46 +0200

    Create headless Linux server

--- hosts/nixos/aio/default.nix
+++ hosts/nixos/aio/default.nix
@@ -68,22 +68,6 @@
     LC_TIME = "pl_PL.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "pl";
-    variant = "";
-  };
-
-  # Configure console keymap
-  console.keyMap = "pl2";
-
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
